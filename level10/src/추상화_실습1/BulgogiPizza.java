71714b17aa4182ac9168ef5127a753df776bb6fb
Lyle <procross27@gmail.com>
2025-01-16 18:18:00 +0900

    feat: abstract exercise 1

---
+++ level10/src/추상화_실습1/BulgogiPizza.java
@@ -0,0 +1,14 @@
+package 추상화_실습1;
+
+public class BulgogiPizza extends Pizza{
+
+
+	public BulgogiPizza(int price, String brand) {
+		super(price, brand, "불고기 피자");
+	}
+
+	@Override
+	void putTopping() {
+		System.out.println("불고기 토핑");
+	}
+}
---
+++ level10/src/추상화_실습1/PizzaService.java
@@ -0,0 +1,14 @@
+package 추상화_실습1;
+
+import java.util.ArrayList;
+
+public class PizzaService {
+	
+	void orderPizzas(ArrayList<Pizza> orderList) {
+		// 배열 -> 같은 타입이 여러개 들어간다.
+		for(Pizza p : orderList) {
+			p.makingPizza();
+			System.out.println("-------------");
+		}
+	}
+}
