9b87483098fec8042e197fbc187f3c2ac4f56cbb
Lyle <procross27@gmail.com>
2025-01-20 10:38:40 +0900

    feat: upload Enumeration

---
+++ level10/src/_10열거타입/PizzaStore.java
@@ -0,0 +1,21 @@
+package _10열거타입;
+
+public class PizzaStore {
+
+	PizzaMenu[] menus = PizzaMenu.values();
+
+	void printAllMenus() {
+		for (PizzaMenu pizza : menus) {
+			System.out.println(pizza);
+		}
+	}
+
+	void takeOrder(String name) {
+		PizzaMenu menu = PizzaMenu.getMenu(name);
+		if (menu == null) {
+			System.out.println("해당 피자는 존재하지 않습니다.");
+			return;
+		}
+		System.out.println(menu.getPrice() + "원입니다.");
+	}
+}
---
+++ level10/src/_10열거타입/_03enum개념3.java
@@ -0,0 +1,37 @@
+package _10열거타입;
+
+import java.util.ArrayList;
+
+public class _03enum개념3 {
+
+	public static void main(String[] args) {
+		
+//		ArrayList<PizzaMenu> list = new ArrayList<PizzaMenu>();
+//		list.add(PizzaMenu.BP);
+//		list.add(PizzaMenu.CP);
+//		list.add(PizzaMenu.HP);
+//		list.add(PizzaMenu.MP);
+//		list.add(PizzaMenu.PP);
+//		list.add(PizzaMenu.SP);
+//		
+//		for(PizzaMenu pizza : list) {
+//			System.out.println(pizza.getDesc());
+//		}
+//		System.out.println("-----------");
+//		
+//		for(PizzaMenu pizza : list) {
+//			System.out.println(pizza.name());
+//		}
+//		System.out.println("-----------");
+//		
+//		for(PizzaMenu pizza : list) {
+//			System.out.println(pizza.ordinal());
+//		}
+//		System.out.println("-----------");
+		PizzaStore store = new PizzaStore();
+		store.printAllMenus();
+		store.takeOrder("마라피자");
+		store.takeOrder("고구마피자");
+	}
+
+}
